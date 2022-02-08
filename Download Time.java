1465e567cfa05798bbdb3f4737ea369c5907996c
VinayakPalmur <99235856+VinayakPalmur@users.noreply.github.com>
2022-02-08 10:31:57 +0530

    Create Download Time.java

---
+++ Download Time.java
@@ -0,0 +1,15 @@
+import java.io.*; // for handling input/output
+import java.util.*; // contains Collections framework
+
+// don't change the name of this class
+// you can add inner classes if needed
+class Main {
+	public static void main (String[] args) {
+                      // Your code here
+					  Scanner sc =new Scanner (System.in);
+					  int n=sc.nextInt();
+					  int t=sc.nextInt();
+					  System.out.println((n*8)/t);
+
+	}
+}
