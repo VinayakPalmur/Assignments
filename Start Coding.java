14a1e18d209787e76a24084286049e7cb9695126
VinayakPalmur <99235856+VinayakPalmur@users.noreply.github.com>
2022-02-08 10:11:08 +0530

    Create Start Coding.java

---
+++ Start Coding.java
@@ -0,0 +1,9 @@
+#include <stdio.h> // header file for Standard Input Output
+#include <stdlib.h> // header file for Standard Library
+
+int main() {
+
+	//Your code here
+	printf("Hello World");
+	return 0;
+}
